ec276265c84d4a79c71e2baf531a2676b0c4b6ae
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2023-11-20 21:27:32 -0600

    set the variables for the rest of the people

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-
 // It's a good idea to declare all your variables.
-// This will let our JS linter help us find 
+// This will let our JS linter help us find
 // spelling errors among other things.
 // Declaring with 'var' will make them global
 // variables.
 
-var aliceSays, bobSays, CarolSays, danSays, ellieSays, frankSays, harrySays, ivySays, jackSays;
-
+var aliceSays,
+  bobSays,
+  carolSays,
+  danSays,
+  ellieSays,
+  frankSays,
+  harrySays,
+  ivySays,
+  jackSays;
 
 // this variable is set to a string.
 let theMessage = "Kathy loves Larry";
@@ -14,7 +20,13 @@ let theMessage = "Kathy loves Larry";
 aliceSays = theMessage;
 bobSays = aliceSays;
 carolSays = bobSays;
-
+danSays = carolSays;
+ellieSays = danSays;
+frankSays = ellieSays;
+gregSays = frankSays;
+harrySays = gregSays;
+ivySays = harrySays;
+jackSays = ivySays;
 
 // call setNames from 'messages.js'
 // to set the messages in the DOM.
